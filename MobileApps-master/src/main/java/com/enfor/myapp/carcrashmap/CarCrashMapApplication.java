08bbf937f86bab3bae21bb7e8c302d28c4a0a3c1
ЯкушевскийВВ <40803132+Vakatr@users.noreply.github.com>
2021-02-08 20:29:14 +0300

    1
    main

---
+++ MobileApps-master/src/main/java/com/enfor/myapp/carcrashmap/CarCrashMapApplication.java
@@ -0,0 +1,13 @@
+package com.enfor.myapp.carcrashmap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarCrashMapApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarCrashMapApplication.class, args);
+	}
+
+}
